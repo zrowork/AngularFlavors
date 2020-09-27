d0f71768d62a87659b20741c2064b90bef17954a
olo <akroworz2@gmail.com>
2020-09-27 14:51:30 +0200

    split two-way data binding

--- src/app/flavors/split-two-way-binding/split-two-way-binding.component.ts
+++ src/app/flavors/split-two-way-binding/split-two-way-binding.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-split-two-way-binding',
   templateUrl: './split-two-way-binding.component.html',
-  styleUrls: ['./split-two-way-binding.component.scss']
+  styleUrls: ['./split-two-way-binding.component.scss'],
 })
 export class SplitTwoWayBindingComponent implements OnInit {
+  userName: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
