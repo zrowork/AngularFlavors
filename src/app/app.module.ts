084e03ed616307781a338ad217112d467167c5f5
olo <akroworz2@gmail.com>
2020-09-27 14:18:18 +0200

    add Binding component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BindingComponent } from './flavors/binding/binding.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BindingComponent
   ],
   imports: [
     BrowserModule,
